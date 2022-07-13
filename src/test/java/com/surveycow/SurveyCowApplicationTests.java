133f44483425f52a38db02caf8bd960903a22b9b
Dinos Kanakakis <dinos.kan@gmail.com>
2022-07-14 01:08:42 +0300

    Initial commit

---
+++ src/test/java/com/surveycow/SurveyCowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surveycow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyCowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
