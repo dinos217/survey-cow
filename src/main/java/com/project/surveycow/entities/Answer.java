5700113be4c1cf40038f101c31f04e0c867d6434
Dinos Kanakakis <dinos.kan@gmail.com>
2022-07-15 01:18:35 +0300

    added files in git

---
+++ src/main/java/com/project/surveycow/entities/Answer.java
@@ -0,0 +1,25 @@
+package com.project.surveycow.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "answer")
+public class Answer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "guestion_id")
+    private Long questionId;
+
+    @Column(name = "question_type")
+    private Long questionType;
+
+    @Column(name = "survey_id")
+    private Long surveyId;
+}
